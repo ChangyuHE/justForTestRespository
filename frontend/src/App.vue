6c3f34577892bcbbab69f3b7849407d2184c9620
Kazakov, Alexey <alexey.kazakov@intel.com>
2020-04-02 20:03:17 +0300

    frontend: detecting selected validations
    Change-Id: Id92672afc736dde6d037d93bf7a3a7ffa5afab31

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,19 +21,37 @@
             </v-list>
         </v-navigation-drawer>
 
-        <v-app-bar app color="teal darken-1" dark>
+        <v-app-bar app color="teal darken-1" dark short>
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
             <v-toolbar-title>GRep tool</v-toolbar-title>
         </v-app-bar>
 
         <v-content>
+            <v-overlay :value="treeLoading">
+                <v-progress-circular indeterminate size="64"></v-progress-circular>
+            </v-overlay>
             <v-container fluid>
-                <v-row align="center" justify="center">
+                <v-row>
                     <v-col>
-                        <v-jstree
-                            :data="data" show-checkbox allow-batch multiple @item-click="itemClick">
+                        <v-jstree ref="tree"
+                            :data="jstreeData" show-checkbox allow-batch multiple @item-click="itemClick">
                         </v-jstree>
                     </v-col>
+                    <v-divider vertical></v-divider>
+                    <v-col class="pa-6" cols="6">
+                        <v-row justify="start" align="start">
+                            <v-list dense flat>
+                                <v-subheader>Selected Validations</v-subheader>
+                                    <v-list-item v-for="(item, i) in selectedValidationsText" :key="i">
+                                        <v-list-item-content>
+                                            <v-list-item-title v-text="item"></v-list-item-title>
+                                        </v-list-item-content>
+                                    </v-list-item>
+                            </v-list>
+                        </v-row>
+                        
+                    </v-col>
+
                 </v-row>
             </v-container>
         </v-content>
@@ -47,39 +65,72 @@
     import VJstree from 'vue-jstree';
     import server from './server.js';
 
+    /**
+     *  returns list of nodes from leaf to root
+     */
+    function getBranchForLeaf(node){
+        let branch = [node];
+        if (node.$children.length == 0) {   // is leaf
+            while (node.$parent.model !== undefined) {      // is root
+                node = node.$parent;
+                branch.push(node);
+            }
+        }
+        return branch;
+    }
+
+
 	export default {
         components: {
             VJstree
         },
         data() {
             return {
-                data: null,
-                loading: true,
+                jstreeData: null,
+                treeLoading: true,
                 errored: false,
                 drawer: false,
+                selectedValidations: [],
+                selectedBranches: []
             }
         },
 		computed: {
-
+            selectedValidationsText(){
+                return this.selectedBranches.map(function(branch){
+                        let texted = branch.reverse().map((node) => (node.model.text));
+                        return `${texted[5]} (${texted[1]}, ${texted[3]}, ${texted[4]})`;
+                    }
+                );
+            }
         },
         methods: {
             itemClick(node) {
-                //console.log(node.model.text + ' clicked !');
-                console.log('node:', node);
+                //console.log('node:', node);
+                this.selectedBranches = [];
+                this.selectedValidations = [];
+
+                this.$refs.tree.handleRecursionNodeChilds(this.$refs.tree,
+                    node => {
+                        if (typeof node.model!='undefined' && node.model.hasOwnProperty('selected') && node.model.selected && node.$children.length == 0) {
+                            this.selectedBranches.push(getBranchForLeaf(node));
+                            this.selectedValidations.push(node.model.id);
+                        }
+                    }
+                )
             }
         },
         beforeCreate() {
             server
                 .get('api/validations/')
                 .then(response => {
-                    console.log(response.data);
-                    this.data = response.data;
+                    //console.log(response.data);
+                    this.jstreeData = response.data;
                 })
                 .catch(error => {
                     console.log(error)
                     this.errored = true
                   })
-                .finally(() => this.loading = false)
+                .finally(() => this.treeLoading = false)
         }
 	}
 </script>

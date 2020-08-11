024ff745cbc9edc6fce9615f9c7426c26398f0cf
Kozlov, Dmitry <dmitry.kozlov@intel.com>
2020-08-11 18:29:55 +0300

    Utility function to get QuerySet of staff e-mails
    Usage:
     get_user_model().staff_emails()
    
    Django application config classes is perfect location to make sure
    that code executed only once.
    
    Change-Id: I6048b8632f0a925852dbb9628a84d0ac84ca6cd9

--- backend/reporting/api/apps.py
+++ backend/reporting/api/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        def staff_emails(cls):
+            return cls.objects \
+                .filter(is_staff=True) \
+                .exclude(email__isnull=True) \
+                .exclude(email__exact='') \
+                .values_list('email', flat=True)
+
+        UserModel = get_user_model()
+        UserModel.add_to_class('staff_emails', classmethod(staff_emails))
